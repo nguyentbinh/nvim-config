95714fbb7f0060f3dea844859894a3b84ce4bdd9
Binh Nguyen <nguyenthaibinh22228@gmail.com>
2022-07-23 00:59:22 -0400

    improve cmp

--- lua/init/cmp.lua
+++ lua/init/cmp.lua
@@ -1,6 +1,11 @@
 -- Setup nvim-cmp.
 local cmp = require'cmp'
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+end
+
 cmp.setup({
     snippet = {
       -- REQUIRED - you must specify a snippet engine
@@ -48,45 +53,25 @@ cmp.setup({
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 
-        ["<Tab>"] = cmp.mapping({
-            c = function()
-              if cmp.visible() then
-                cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
-              else
-                cmp.complete()
-              end
-            end,
-            i = function(fallback)
-              if cmp.visible() then
-                cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
-              else
-                fallback()
-              end
-            end,
-            s = function(fallback)
+        ['<Tab>'] = function(fallback)
+          if not cmp.select_next_item() then
+            if vim.bo.buftype ~= 'prompt' and has_words_before() then
+              cmp.complete()
+            else
               fallback()
             end
-          }),
+          end
+        end,
 
-        ["<S-Tab>"] = cmp.mapping({
-            c = function()
-              if cmp.visible() then
-                cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
-              else
-                cmp.complete()
-              end
-            end,
-            i = function(fallback)
-              if cmp.visible() then
-                cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
-              else
-                fallback()
-              end
-            end,
-            s = function(fallback)
+        ['<S-Tab>'] = function(fallback)
+          if not cmp.select_prev_item() then
+            if vim.bo.buftype ~= 'prompt' and has_words_before() then
+              cmp.complete()
+            else
               fallback()
             end
-          }),
+          end
+        end,
 
         ['<Down>'] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), {'i'}),
         ['<Up>'] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), {'i'}),
@@ -146,7 +131,6 @@ cmp.setup({
 cmp.setup.filetype('gitcommit', {
     sources = cmp.config.sources({
         -- { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-      }, {
         { name = 'buffer' },
       })
   })
@@ -155,17 +139,16 @@ cmp.setup.filetype('gitcommit', {
 cmp.setup.cmdline('/', {
     mapping = cmp.mapping.preset.cmdline(),
     sources = {
-      { name = 'buffer' }
-    }
+        { name = 'buffer' },
+      }
   })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
     mapping = cmp.mapping.preset.cmdline(),
     sources = cmp.config.sources({
-        { name = 'path' }
-      }, {
-        { name = 'cmdline' }
+        { name = 'path' },
+        { name = 'cmdline' },
       })
   })
 
